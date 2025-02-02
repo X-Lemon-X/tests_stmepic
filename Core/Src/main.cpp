af00c1bcda48c2c4015cd31e5636dad58a215f1b
Patryk Dudzinski (LemonX) <dudzinski.patryk.02@gmail.com>
2025-02-02 18:42:19 +0100

    update to freertos

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -6,7 +6,7 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2024 STMicroelectronics.
+  * Copyright (c) 2025 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -18,10 +18,11 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "cmsis_os.h"
+#include "main_prog.hpp"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "main_prog.hpp"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -53,6 +54,13 @@ UART_HandleTypeDef huart1;
 
 HCD_HandleTypeDef hhcd_USB_OTG_FS;
 
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -67,6 +75,8 @@ static void MX_USART1_UART_Init(void);
 static void MX_TIM10_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USB_OTG_FS_HCD_Init(void);
+void StartDefaultTask(void *argument);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -113,8 +123,44 @@ int main(void)
   MX_I2C1_Init();
   MX_USB_OTG_FS_HCD_Init();
   /* USER CODE BEGIN 2 */
-  main_prog();
+
   /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+main_prog();
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
@@ -458,6 +504,45 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
 
 /**
   * @brief  This function is executed in case of error occurrence.
--- src/main_prog.cpp
+++ src/main_prog.cpp
@@ -1,14 +1,12 @@
 #include "stmepic.hpp"
 #include "movement_controler.hpp"
 #include "Timing.hpp"
-#include "can_control.hpp"
-#include "can_control.hpp"
 #include "steper_motor.hpp"
 #include "encoder_magnetic.hpp"
 #include "memory_fram.hpp"
 #include "filter.hpp"
 #include "main.h"
-#include "pin.hpp"
+#include "gpio.hpp"
 #include "movement_controler.hpp"
 #include "controler_linear.hpp"
 #include "encoder_magnetic.hpp"
@@ -17,6 +15,9 @@
 #include <memory>
 #include <vector>
 #include "fram_i2c.hpp"
+#include "logger.hpp"
+#include "can.hpp"
+#include "i2c.hpp"
 
 extern CAN_HandleTypeDef hcan1;
 
@@ -36,11 +37,11 @@ extern HCD_HandleTypeDef hhcd_USB_OTG_FS;
 using  namespace stmepic;
 
 
-gpio::GpioPin direction_pin={GPIO_PIN_0, GPIOB, 0};
-gpio::GpioPin enable_pin={GPIO_PIN_1, GPIOB, 0};
-gpio::GpioPin step_pin={GPIO_PIN_2, GPIOB, 0};
-
-
+GpioPin direction_pin(*GPIOB,GPIO_PIN_0);
+GpioPin enable_pin(*GPIOB,GPIO_PIN_1);
+GpioPin step_pin(*GPIOB,GPIO_PIN_2);
+GpioPin pin_i2c1_sda(*GPIOB, GPIO_PIN_7);
+GpioPin pin_i2c1_scl(*GPIOB, GPIO_PIN_6);
 
 
 
@@ -51,52 +52,49 @@ motor::SteperMotorStepDir motorStepDir(htim10,TIM_CHANNEL_1, direction_pin, enab
 motor::MotorClosedLoop motorClosedLoop(motorStepDir,&encoderAbsoluteMagnetic,&encoderAbsoluteMagnetic,nullptr);
 TimeScheduler timeScheduler(Ticker::get_instance());
 filters::FilterSampleSkip fss;
-CanControl<> canControl;
 memory::FRAM *fram= new memory::FramI2C(hi2c1,0x60,0x0,0xffff);
 
+std::shared_ptr<stmepic::I2C> i2c1;
+std::shared_ptr<stmepic::CAN> can1;
 
 
-void test_movement_controler(){
-  movementControler.init(motorClosedLoop, movement::MovementControlMode::POSITION, basicLinearPosControler);
-  movementControler.set_enable(false);
-}
-
-void task1(Timing& t){
-  movementControler.handle();
-
-}
-
-void task2(Timing& t){
-  encoderAbsoluteMagnetic.handle();
-}
-
-void can_callback(CanMsg& msg){
+void can_callback(stmepic::CAN&can, CanDataFrame& frame, void *arg) {
   __NOP();
 }
 
 void main_prog() {
-  canControl.init(hcan1, 0, nullptr, nullptr);
-  canControl.add_callback(CanControl<>::CAN_DEFAULT_FRAME_ID, can_callback);
-  fram->device_start();
-  encoderAbsoluteMagnetic.device_start();
+   CAN_FilterTypeDef can_filter;
+  can_filter.FilterBank           = 1;
+  can_filter.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+  can_filter.FilterActivation     = CAN_FILTER_ENABLE;
+  can_filter.FilterMode           = CAN_FILTERMODE_IDMASK;
+  can_filter.FilterScale          = CAN_FILTERSCALE_16BIT;
+  can_filter.FilterIdHigh         = 0;
+  can_filter.FilterIdLow          = 0;
+  can_filter.FilterMaskIdHigh     = 0;
+  can_filter.FilterMaskIdLow      = 0;
+  can_filter.SlaveStartFilterBank = 0;
+  auto mayby_can1 = stmepic::CAN::Make(hcan1,can_filter);
+  if(!mayby_can1.ok()) {
+    log_error("CAN1 error: " + mayby_can1.status().to_string());
+    HAL_NVIC_SystemReset();
+  }
+  can1 = mayby_can1.valueOrDie();
 
-  encoderAbsoluteMagnetic.init(hi2c1, encoders::translate_reg_to_angle_AS5600, &fss, &fss);
-  auto s = stmepic::Timing::Make(
-    100,
-    true,
-    task1,
-    Ticker::get_instance()
-  );
-  timeScheduler.add_timer(s.valueOrDie());
-  s = stmepic::Timing::Make(
-    100,
-    true,
-    task2,
-    Ticker::get_instance()
-  );
-
-  timeScheduler.add_timer(s.valueOrDie());
+  auto mayby_i2c1 = stmepic::I2C::Make(hi2c1,pin_i2c1_sda,pin_i2c1_scl,HardwareType::BLOCKING);
+  if(!mayby_i2c1.ok()) {
+    log_error("I2C1 error: " + mayby_i2c1.status().to_string());
+    HAL_NVIC_SystemReset();
+  }
+  i2c1 = mayby_i2c1.valueOrDie();
 
 
-  timeScheduler.schedules_handle_blocking();
+  can1->add_callback(0x123,can_callback);
+  can1->hardware_start();
+  i2c1->hardware_start();
+
+  fram->device_enable();
+  encoderAbsoluteMagnetic.init(i2c1, encoders::translate_reg_to_angle_AS5600, &fss, &fss);
+  encoderAbsoluteMagnetic.device_enable();
+  encoderAbsoluteMagnetic.device_task_start();
 }
